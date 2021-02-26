82eea485281e908d61f9fb143db07c6cea0fcba7
jiladahe1997 <972931182@qq.com>
2021-02-26 15:09:00 +0800

    modify: 更新音频初始化配置。由于之前的音频初始化时经常会在dmesg中报错，修改了顺序。         目前报错几率有所减少，但是还是会报错。

--- board/renmingrui/rootfs_overlay/root/Eplayer.sh
+++ board/renmingrui/rootfs_overlay/root/Eplayer.sh
@@ -5,29 +5,76 @@
 echo "Eplayer 守护器开始"
 echo "等待网络ok"
 
-wwanStatus=0;
+#wwanStatus=0;
 export TSLIB_TSDEVICE=/dev/input/event1
 export QT_QPA_EVDEV_TOUCHSCREEN_PARAMETERS=/dev/input/event1
 export QT_QPA_FB_TSLIB=1
-amixer sset Headphone 127,127
-amixer sset 'Right Output Mixer PCM' on
-amixer sset 'Left Output Mixer PCM' on
-amixer sset Speaker 127,127
-amixer sset Playback 236,236
+
 #alsactl --file ~/.config/asound.state restore
-while [ $wwanStatus -eq 0 ]
-do
-    wwanStatus=$(cat /sys/class/net/wwan0/carrier)
-    echo "当前网络状态 $wwanStatus"
-    sleep 5
-done
+#while [ $wwanStatus -eq 0 ]
+#do
+    #wwanStatus=$(cat /sys/class/net/wwan0/carrier)
+    #echo "当前网络状态 $wwanStatus"
+    #sleep 5
+#done
 
 echo "网络ok"
+
+amixerStatus=""
+sleep 5s
+
+#while [ "$amixerStatus" = "" ]
+#do
+#	echo "amixer sset Headphone 101,101"
+#	amixerStatus=$(amixer sset Headphone 101,101)
+#	echo "amixerStatus: $amixerStatus"
+#	sleep 2s
+#done
+#amixerStatus=""
+
+while [ "$amixerStatus" = "" ]
+do
+	echo "amixer sset Speaker 116,116"
+	amixerStatus=$(amixer sset Speaker 116,116)
+	echo "amixerStatus: $amixerStatus"
+	sleep 2s
+done
+amixerStatus=""
+
+while [ "$amixerStatus" = "" ]
+do
+	echo "amixer sset Playback 250,250"
+	amixerStatus=$(amixer sset Playback 250,250)
+	echo "amixerStatus: $amixerStatus"
+	sleep 2s
+done
+amixerStatus=""
+
+while [ "$amixerStatus" = "" ]
+do
+	echo "amixer sset 'Right Output Mixer PCM' on"
+	amixerStatus=$(amixer sset 'Right Output Mixer PCM' on)
+	echo "amixerStatus: $amixerStatus"
+	sleep 2s
+done
+amixerStatus=""
+
+while [ "$amixerStatus" = "" ]
+do
+	echo "amixer sset 'Left Output Mixer PCM' on"
+	amixerStatus=$(amixer sset 'Left Output Mixer PCM' on)
+	echo "amixerStatus: $amixerStatus"
+	sleep 2s
+done
+amixerStatus=""
+
+echo "音频ok"
 while true
 do
     if pgrep -x "Eplayer" > /dev/null
     then
-        echo "Eplayer running"
+        #echo "Eplayer running"
+		:
     else
         echo "Eplayer Stopped,ready to restart"
         /root/Eplayer --platform linuxfb &
